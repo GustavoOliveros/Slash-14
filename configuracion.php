9f8eaa4d87c04196990145e17ba86b66c7ecd968
GustavoOliveros <gusa05+1@gmail.com>
2022-11-06 21:11:48 -0300

    Primer commit

---
+++ configuracion.php
@@ -0,0 +1,22 @@
+<?php 
+
+header('Content-Type: text/html; charset=utf-8');
+header ("Cache-Control: no-cache, must-revalidate ");
+
+/////////////////////////////
+// CONFIGURACION APP//
+/////////////////////////////
+
+// Ubicación del Proyecto
+$PROYECTO ='PWD/Slash-14';
+$ROOT =$_SERVER['DOCUMENT_ROOT']."/$PROYECTO/";
+include_once($ROOT.'Util/funciones.php');
+$GLOBALS['ROOT']=$ROOT;
+
+// Página de Autenticación
+$INICIO = "Location:http://".$_SERVER['HTTP_HOST']."/$PROYECTO/vista/login/login.php";
+
+// Página Principal
+$PRINCIPAL = "Location:http://".$_SERVER['HTTP_HOST']."/$PROYECTO/home/index.php";
+
+?>
---
+++ readme.md
@@ -0,0 +1,13 @@
+# SLASH 14
+Aplicación de compra web realizada en PHP. Incluye carrito y módulo de autenticación de usuario.
+
+## Materia 2022
+- Programación Web Dinámica
+- Tecnicatura Universitaria en Desarrollo Web
+- Universidad del Comahue
+
+## Grupo #6
+- Gustavo Oliveros
+- Gonzalo Olmos
+- Jose Centena
+
